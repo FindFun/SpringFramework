f97215448898456ca7b38143cede0fe8c5aea70f
funfind <funfind@yeah.net>
2020-02-15 02:30:11 +0800

    Test idea commit

--- shadow/src/main/java/com/luban/service/Test.java
+++ shadow/src/main/java/com/luban/service/Test.java
@@ -12,5 +12,6 @@ public class Test {
 
 		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(Appconfig.class);
 		ac.getBean(lubanService.class).xx();
+		System.out.println("adf");
 	}
 }
